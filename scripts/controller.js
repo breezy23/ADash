a2c40ede499d94649d54620853e51b136e1361d5
breezy23 <brz0323@gmail.com>
2022-12-11 22:54:16 -0500

    Layed out ribbon header

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,7 +1,23 @@
-const url = "https://api.open-meteo.com/v1/forecast?latitude=42.03&longitude=-93.62&daily=weathercode,temperature_2m_max,sunrise,sunset,precipitation_sum&temperature_unit=fahrenheit&windspeed_unit=mph&timezone=America%2FChicago"
+const fc = "https://api.open-meteo.com/v1/forecast?latitude=42.03&longitude=-93.62&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago";
+const wa = "https://api.weather.gov/alerts/active?point=41.99,-93.62";
 
-fetch(url)
-    .then(res => res.json())
-    .then((out) => {
-        console.log('Output: ', out);
+let weather;
+let weatherAlert;
+
+console.log("test");
+// Get response from Open-Meteo
+fetch(fc)
+	.then(res => res.json())
+	.then((out) => {
+		weather = out;
+	    	console.log('Forecast: ', weather);
+}).catch(err => console.error(err));
+
+// Get weather alerts from the government
+// It's tin hat time! :D
+fetch(wa)
+     	.then(res => res.json())
+	.then((out) => {
+		weatherAlert = out;
+		console.log('Alerts: ', weatherAlert);
 }).catch(err => console.error(err));
