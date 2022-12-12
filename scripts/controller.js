ff5f02db7ba7c1e697d5365c0429a9a3e334436d
breezy23 <brz0323@gmail.com>
2022-12-12 00:35:55 -0500

    Added time

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,10 +1,16 @@
+// This class controls the dash, which is why it's called controller! :D
+// Basically a lot of definitions and timers
+
+// URL Apis
+// One for forecast and another for weather alerts! :D
 const fc = "https://api.open-meteo.com/v1/forecast?latitude=42.03&longitude=-93.62&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago";
 const wa = "https://api.weather.gov/alerts/active?point=41.99,-93.62";
 
 let weather;
 let weatherAlert;
 
-console.log("test");
+init();
+
 // Get response from Open-Meteo
 fetch(fc)
 	.then(res => res.json())
@@ -21,3 +27,13 @@ fetch(wa)
 		weatherAlert = out;
 		console.log('Alerts: ', weatherAlert);
 }).catch(err => console.error(err));
+
+// Start a lot of timers
+// 1 second timer
+setInterval(() => {
+	updateTime();
+}, 1000);
+
+function init() {
+	updateTime();
+}
---
+++ scripts/time.js
@@ -0,0 +1,36 @@
+const m = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const w = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const t = document.getElementById("t_value");
+const s = document.getElementById("t_suffix");
+const d = document.getElementById("date");
+
+function updateTime() {
+	const dte = new Date();
+	const month = dte.getMonth();
+	const date = dte.getDate();
+	const day = dte.getDay();
+	const year = dte.getFullYear();
+
+	t.innerHTML = undefined;
+	d.innerHTML = undefined;
+
+	let suffix;
+	let hour = dte.getHours();
+	let minute = dte.getMinutes().toString();
+
+	if(hour < 12) {
+		suffix = "AM";
+	} else {
+		suffix = "PM";
+		hour -= 12;
+	}
+
+	if(minute.length < 2) {
+		minute = "0"+minute;
+	}
+
+	t.textContent = hour+":"+minute;
+	s.textContent = suffix;
+	d.textContent = w[day]+", "+m[month]+" "+date+", "+year;
+}
