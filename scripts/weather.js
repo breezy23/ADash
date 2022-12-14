c4b4af94e689a74fa4c2450805108df0f7211ed3
breezy23 <brz0323@gmail.com>
2022-12-13 21:23:11 -0500

    Added basic weather functionality

--- scripts/weather.js
+++ scripts/weather.js
@@ -5,6 +5,7 @@ const w0 = document.getElementById("weather0");
 const w1 = document.getElementById("weather1");
 const w2 = document.getElementById("weather2");
 
+let condition
 let weather;
 
 // There is *probably* a better way to do this but oh well
@@ -12,6 +13,7 @@ function buildWeather() {
 	const date = new Date();
 	const res = weather;
 	const daily = res.daily;
+	const current = res.current_weather;
 	let temp;
 	let code;
 	let day;
@@ -32,6 +34,12 @@ function buildWeather() {
 
 		getStyles(daily, i);
 	}
+
+	// Create current weather
+	// Should have thought ahead oh well
+	document.getElementById("c_icon").className = getStyles(undefined, current.weathercode) + " current_icon";
+	document.getElementById("c_condition").innerHTML = condition;
+	document.getElementById("c_temp").innerHTML = current.temperature+"\u00B0F";
 }
 
 // Get response from open-meteo
@@ -47,47 +55,72 @@ function getWeather() {
 
 // :(
 function getStyles(daily, index) {
-	let code = daily.weathercode[index];
+	let code;
+	// Awful garbage trash ew ew
+	if (daily != undefined ) {
+		code = daily.weathercode[index];
+	} else {
+		code = index;
+	}
+
 	let styles = "wi ";
 
 	// Not an effeicient way, but I'm a simple man who doesn't
 	// understand the nuances of weathercodes
 	if(code < 3) {
 		styles += "wi-day-sunny";
+		condition = "Taste the sun!";
 	} else if (code < 20) {
 		styles += "wi-day-cloudy";
+		condition = "Cloudy";
 	} else if (code < 22) {
 		styles += "wi-day-sprinkle";
+		condition = "Wet";
 	} else if (code == 22) {
 		styles += "wi-day-snow";
+		condition = "Snow???";
 	} else if (code < 25) {
 		styles += "wi-day-rain-mix";
+		condition = "Solid rain?";
 	} else if (code == 25) {
 		styles += "wi-day-rain";
+		condition = "It's raining.";
 	} else if (code < 28) {
 		styles += "wi-day-rain-mix";
+		condition = "Rain but also solid"
 	} else if (code == 28) {
 		styles += "wi-day-fog";
+		condition = "Foggy :o"
 	} else if (code == 29) {
 		styles += "wi-day-thunderstorm";
+		condition = "Thunderstorm! :D";
 	} else if (code < 40) {
 		styles += "wi-day-snow-wind";
+		condition = "Snowing! :D"
 	} else if (code < 50) {
 		styles += "wi-day-fog";
+		condition = "Foggy :o";
 	} else if (code < 60) {
 		styles += "wi-day-sprinkle";
+		condition = "Sprinkling";
 	} else if (code < 70) {
 		styles += "wi-day-rain";
+		condition = "Raining";
 	} else if (code < 80) {
 		styles += "wi-day-snow-wind";
+		condition = "Snowing";
 	} else if (code < 83) {
 		styles += "wi-day-rain-wind";
+		condition = "Raining";
 	} else if (code < 85) {
 		styles += "wi-day-rain-mix";
+		condition = "Rainy mix";
 	} else if (code < 91) {
 		styles += "wi-day-snow-wind";
+		condition = "Snowing";
 	} else {
 		styles += "wi-day-thunderstorms";
+		condition = "Thunderstorms";
 	}
 
 	return styles;
